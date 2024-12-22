8c7b819a8eca0a69b75a5f56ffcf744666e3bb78
TobyM27 <tobyanto.121140099@student.itera.ac.id>
2024-12-22 20:38:19 +0700

    [refactor] update comments serta docstrings dalam main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Mengimport library yang diperlukan untuk tugas besar ini
 import sys
 import numpy as np
 import cv2
@@ -18,27 +19,26 @@ from utils.heart_rate import cpu_POS
 
 def download_model_face_detection():
     """
-    Mengunduh model pose landmarker dari MediaPipe.
-    Model ini digunakan untuk mendeteksi pose tubuh dalam video.
+    Mengunduh model face_detector dari MediaPipe.
+    Model ini digunakan untuk mendeteksi muka pengguna dalam video.
     """
-    # Create models directory if it doesn't exist
+    # Membuat direktori model jika belum ada 
     model_dir = "model"
     os.makedirs(model_dir, exist_ok=True)
     
     url = "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/latest/blaze_face_short_range.tflite"
     filename = os.path.join(model_dir, "face_detector.task")
     
-    # Check if file already exists and is not empty
+    # Memeriksa apabila file sudah terbuat dan tidak kosong
     if os.path.exists(filename) and os.path.getsize(filename) > 0:
-        print(f"Model file {filename} already exists and is valid, skipping download.")
+        print(f"File model {filename} sudah terunduh. Melewati proses pengunduhan...")
         return filename
     
-    # Download with progress bar
+    # Proses pengunduhan dengan progress bar (yang menggunakan library tqdm)
     try:
-        print(f"Downloading model to {filename}...")
+        print(f"Mengunduh model face detector ke direktori: {filename}...")
         response = requests.get(url, stream=True)
         response.raise_for_status()
-        
         total_size = int(response.headers.get('content-length', 0))
         block_size = 1024
         
@@ -54,18 +54,22 @@ def download_model_face_detection():
         
         # Verify downloaded file
         if os.path.getsize(filename) == 0:
-            raise ValueError("Downloaded file is empty")
+            raise ValueError("File yang terunduh kosong")
             
-        print("Download completed successfully!")
+        print("Proses pengunduhan model face detector berhasil!")  
         return filename
-        
+    
     except Exception as e:
-        print(f"Error downloading the model: {e}")
+        print(f"Terdapat error dalam proses pengunduhan model face detector: {e}")
         if os.path.exists(filename):
             os.remove(filename)  # Clean up partial download
         raise
 
 def download_model_pose_detection():
+    """
+    Mengunduh model pose landmarker dari MediaPipe.
+    Model ini digunakan untuk mendeteksi pose tubuh pengguna dalam video (khususnya dalam bagian bahu / torso atas).
+    """
     model_dir = "model"
     os.makedirs(model_dir, exist_ok=True)
     
@@ -73,11 +77,11 @@ def download_model_pose_detection():
     filename = os.path.join(model_dir, "pose_landmarker.task")
     
     if os.path.exists(filename) and os.path.getsize(filename) > 0:
-        print(f"Model file {filename} already exists and is valid, skipping download.")
+        print(f"File model {filename} sudah terunduh. Melewati proses pengunduhan...")
         return filename
     
     try:
-        print(f"Downloading model to {filename}...")
+        print(f"Mengunduh model pose landmarker ke direktori: {filename}...")
         response = requests.get(url, stream=True)
         response.raise_for_status()
         
@@ -95,13 +99,13 @@ def download_model_pose_detection():
                 pbar.update(size)
         
         if os.path.getsize(filename) == 0:
-            raise ValueError("Downloaded file is empty")
+            raise ValueError("File yang terunduh kosong")
             
-        print("Download completed successfully!")
+        print("Proses pengunduhan model pose landmarker berhasil!")
         return filename
         
     except Exception as e:
-        print(f"Error downloading the model: {e}")
+        print(f"Terdapat error dalam proses pengunduhan model face detector: {e}")
         if os.path.exists(filename):
             os.remove(filename)
         raise
@@ -115,56 +119,78 @@ def check_gpu():
     """
     system = platform.system()
     print(f"System: {system}")
-    # Check for NVIDIA GPU
+    # Memeriksa apakah sistem adalah Windows atau Linux 
     if system == "Linux" or system == "Windows":
         try:
             nvidia_output = subprocess.check_output(['nvidia-smi']).decode('utf-8')
-            return "NVIDIA"
+            return "NVIDIA" 
         except (subprocess.CalledProcessError, FileNotFoundError):
             return "CPU"
 
-    # Check for Apple MLX
+    # Memeriksa apakah sistem adalah macOS dengan arsitektur Apple Silicon
     elif system == "Darwin":  # macOS
         try:
             cpu_info = subprocess.check_output(['sysctl', '-n', 'machdep.cpu.brand_string']).decode('utf-8').strip()
             print(f"CPU: {cpu_info}")
-            if "Apple" in cpu_info:  # This indicates Apple Silicon (M1/M2/M3)
+            if "Apple" in cpu_info:  # Ini mencakupi semua chip buatan Apple (M1/M2/M3)
                 return "MLX"
         except subprocess.CalledProcessError:
             pass
-    return "CPU"
+    return "CPU" # Menggunakan CPU sebagai default
 
 ## The length to be around should width to ensure the landmarks are detected and can proceed to optical flow 
 def get_initial_roi(image, landmarker, x_size=100, y_size=30, shift_x=0, shift_y=-30):
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    height, width = image.shape[:2]
+    """
+    Mengambil ROI dari webcam untuk mendeteksi sinyal respirasi berdasarkan pergerakan posisi bahu pasien.
+
+    Args:
+        image (np.ndarray): Frame dari webcam
+        pose_landmarker (task): MediaPipe pose detector yang sudah didownload
+        x_size (int): Ukuran ROI pada sumbu x
+        y_size (int): Ukuran ROI pada sumbu y
+        shift_x (int): Pergeseran ROI pada sumbu x (dimana nilai positif akan menggeser ROI ke kanan dan sebaliknya)
+        shift_y (int): Pergeseran ROI pada sumbu y (dimana nilai positif akan menggeser ROI ke bawah dan sebaliknya)
+
+    Returns:
+        tuple: Koordinat ROI (left_x, top_y, right_x, bottom_y)
+    """
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Mengubah warna BGR ke RGB
+    height, width = image.shape[:2] # Mengambil dimensi frame webcam
     
+    # Membuat gambar MediaPipe dari frame webcam
     mp_image = mp.Image(
         image_format=mp.ImageFormat.SRGB,
         data=image_rgb
     )
     
+    # Mendeteksi pose dari frame webcam
     detection_result = landmarker.detect(mp_image)
     
     if not detection_result.pose_landmarks:
         raise ValueError("No pose detected in first frame!")
-        
+    
+    # Mendeteksi tubuh pengguna dari landmark pertama
     landmarks = detection_result.pose_landmarks[0]
     
+    # Mengambil landmark bahu kiri dan kanan
     left_shoulder = landmarks[11]
     right_shoulder = landmarks[12]
     
+    # Menghitung posisi tengah dari bahu kiri dan bahu kanan
     center_x = int((left_shoulder.x + right_shoulder.x) * width / 2)
     center_y = int((left_shoulder.y + right_shoulder.y) * height / 2)
     
+    # Mengaplikasikan shift terhadap titik tengah
     center_x += shift_x
     center_y += shift_y
     
+    # Manghitung batasan ROI berdasarkan posisi tengah dan ukuran ROI
     left_x = max(0, center_x - x_size)
     right_x = min(width, center_x + x_size)
     top_y = max(0, center_y - y_size)
     bottom_y = min(height, center_y + y_size)
     
+    # Mevalidasi ukuran ROI
     if (right_x - left_x) <= 0 or (bottom_y - top_y) <= 0:
         raise ValueError("Invalid ROI dimensions")
         
